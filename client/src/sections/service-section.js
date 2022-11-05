16f16b051c415e83302672af5a158f4f86b93bae
SimerusM <63352151+SimerusM@users.noreply.github.com>
2022-11-05 15:04:55 -0400

    updated video

--- client/src/sections/service-section.js
+++ client/src/sections/service-section.js
@@ -90,7 +90,7 @@ export default function ServiceSection() {
       <ModalVideo
         channel="youtube"
         isOpen={videoOpen}
-        videoId="ZNA9rmDsYVE"
+        videoId="L12RWt1UEWA"
         onClose={() => setVideoOpen(false)}
       />
     </section>
